ad00dbdb69cfda3a84515fb1954ebad4c847043a
unknown <duankai@jk.pingan-inc.cn>
2016-06-27 15:52:26 +0800

    first commit, this is data view

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 4500
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, './client/index.html'))
+})
+app.use('/static/', express.static(path.join(__dirname, './dist')))
+app.listen(port, function(err){
+  if(err){
+    throw new Error(err)
+  }else{
+    console.log(`server listen on:  ${port}`)
+  }
+})
